69b156c7aa80f9a5593f435bd329c0fbec2e6b0e
rohitkuma6443 <56398441+rohitkuma6443@users.noreply.github.com>
2023-10-23 12:12:25 +0000

    Opreation Added

---
+++ Lec_04/Operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(5%3)
+
+// let str1 = "hello"
+// let str2 = "Hitesh"
+
+// let str3 = str1+str2
+
+// console.log(str3)
+
+// console.log("1" + 2)  --- 12
+// console.log(1+"2")    --- 12
+// consloe.log("1"+2+2)  --- 122 
+// console.log(1+2+"3")  --- 32
